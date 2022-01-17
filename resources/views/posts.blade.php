428507b4da76146f04856adccdac54201f9191f4
Pavel <parbeck17@gmail.com>
2022-01-17 21:03:01 +0200

    lesson 7

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+    <script src="/script.js"></script>
+</head>
+<body>
+    <h1>My blog</h1>
+
+    <article>
+        <h2>My first post</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut provident optio dolorum mollitia laborum aperiam pariatur rem ad similique molestias cum voluptatem blanditiis saepe ratione voluptas veritatis, perspiciatis dolor perferendis!</p>
+        <a href="/post">Read more</a>
+    </article>
+    <article>
+        <h2>My second post</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut provident optio dolorum mollitia laborum aperiam pariatur rem ad similique molestias cum voluptatem blanditiis saepe ratione voluptas veritatis, perspiciatis dolor perferendis!</p>
+        <a href="/post">Read more</a>
+    </article>
+    <article>
+        <h2>My third post</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut provident optio dolorum mollitia laborum aperiam pariatur rem ad similique molestias cum voluptatem blanditiis saepe ratione voluptas veritatis, perspiciatis dolor perferendis!</p>
+        <a href="/post">Read more</a>
+    </article>
+</body>
+</html>
