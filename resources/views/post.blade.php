fff79171fc7b0a5a546d57a33a712651d0a59887
Pavel <parbeck17@gmail.com>
2022-01-18 15:50:22 +0200

    layaots, x-layaots, extend, section

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My blog</title>
-    <link rel="stylesheet" href="/app.css">
-    <script src="/script.js"></script>
-</head>
-<body>
+<x-layaots>
     <h1>My blog</h1>
 
     <article>
-        <h1><?= $post->title ?></h1>
-        <p><em>{{ $post->date }}</em></p>
+        <h1>{{ $post->title }}</h1>
         <div>
-            <?= $post->body ?>
+            {!! $post->body !!}
         </div>
     </article>
     <a href="/">Go back</a>
-</body>
-</html>
+</x-layaots>
