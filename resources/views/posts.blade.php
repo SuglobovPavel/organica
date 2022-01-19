d3cd3ae56cba818ba7c55a69b1a1b7c7a28c74c6
Pavel <parbeck17@gmail.com>
2022-01-19 15:07:54 +0200

    lesson 25 - views on front page categories

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
     <article class="my-class-{{$loop->index}} my-class-{{ $loop->even ? 'even': '' }}">
         <h2>{{ $post->title }}</h2>
         <p>{{ $post->excerpt }}</p>
-        <p>Category:   <a href="{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>Category:   <a href="categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         <a href="/posts/{{ $post->slug }}">Read more</a>
     </article>
     <?php endforeach ?>
