0b0ae41b8000383a002d521841250378bab80bd4
Pavel <parbeck17@gmail.com>
2022-01-20 17:56:49 +0200

    Use seed and create user and create relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User;
+
 class Post extends Model
 {
     use HasFactory;
@@ -13,5 +15,9 @@ class Post extends Model
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,6 +6,7 @@
     <article class="my-class-{{$loop->index}} my-class-{{ $loop->even ? 'even': '' }}">
         <h2>{{ $post->title }}</h2>
         <p>{{ $post->excerpt }}</p>
+        <p>Author by <a href="#">{{ $post->user->name }}</a></p>
         <p>Category:   <a href="categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         <a href="/posts/{{ $post->slug }}">Read more</a>
     </article>
