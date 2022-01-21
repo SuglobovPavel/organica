6dd77ac1b21f468f43c66a5ddffaa0946d9d5714
Pavel <parbeck17@gmail.com>
2022-01-21 11:43:27 +0200

    factrory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,47 +18,15 @@ class DatabaseSeeder extends Seeder
     {
         User::truncate();
         Category::truncate();
+        Post::truncate();
 
-        $user = User::factory()->create();
-
-        $category_1 = Category::create([
-            'name' => 'categoy 1',
-            'slug' => 'category_1'
-        ]);
-        $category_2 = Category::create([
-            'name' => 'categoy 2',
-            'slug' => 'category_2'
-        ]);
-        $category_3 = Category::create([
-            'name' => 'categoy 3',
-            'slug' => 'category_3'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $category_1->id,
-            'title' => 'My first post',
-            'slug' => 'my-first-post',
-            'except' => 'description my first post',
-            'body' => 'text for my first post'
-        ]);
-        
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $category_2->id,
-            'title' => 'My second post',
-            'slug' => 'my-second-post',
-            'except' => 'description my second post',
-            'body' => 'text for my second post'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $category_3->id,
-            'title' => 'My third post',
-            'slug' => 'my-third-post',
-            'except' => 'description my third post',
-            'body' => 'text for my third post'
-        ]);
     }
 }
